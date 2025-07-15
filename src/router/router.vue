a2f5e29dc42b9ecf4e97d86a25cdf389310d2fcf
MYN <yn.moon@hanil.com>
2025-07-15 18:02:25 +0900

    routing 작업 중

---
+++ src/router/router.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+
+import { onMounted } from 'vue'
+import router from './router'
+
+onMounted(() => {
+  router.push('/home');
+});
+</script>
+
